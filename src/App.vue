ceb579e11b314a5924b0ec51b16a8431a33b70d1
renzo <renzo.carara@libero.it>
2020-09-30 22:20:11 +0200

    release iniziale

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <HeaderComp />
+     <transition name="fade" mode="out-in" appear>
+        <router-view />
+     </transition>
+     <FooterComp />
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from "@/components/HeaderComp.vue";
+import FooterComp from "@/components/FooterComp.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HeaderComp,
+        FooterComp,
+    },
+};
 </script>
 
+
 <style>
+body {
+    background-color: #f0f0f0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+.container {
+    margin-top: 230px;
+    padding: 20px 0;
+    background-color: #f0f0f0;
+}
+/* // --------------------------- TRANSITIONs ----------------------------------- */
+/* // transizione in ingresso, per apparire gradualmente */
+.fade-enter-active {
+    transition: opacity 1.5s;
+}
+.fade-enter {
+    opacity: 0;
+}
+
+/* // transizione in uscita, per scomparire gradualmente */
+.fade-leave-active {
+    transition: opacity 0.5s;
+}
+.fade-leave-to {
+    opacity: 0;
 }
 </style>
